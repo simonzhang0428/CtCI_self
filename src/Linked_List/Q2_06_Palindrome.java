64472e99c7ddc0ece2d1ae4bd739a5e14745377f
simon <simonzhang0428@gmail.com>
2021-03-14 00:20:52 -0800

    clean up

--- src/Linked_List/Q2_06_Palindrome.java
+++ src/Linked_List/Q2_06_Palindrome.java
@@ -45,13 +45,35 @@ public class Q2_06_Palindrome {
         }
         return one == null && two == null;
     }
+    public static void main(String[] args) {
+        int length = 9;
+        LinkedListNode[] nodes = new LinkedListNode[length];
+        for (int i = 0; i < length; i++) {
+            nodes[i] = new LinkedListNode(i >= length / 2 ? length - i - 1 : i, null, null);
+        }
+
+        for (int i = 0; i < length; i++) {
+            if (i < length - 1) {
+                nodes[i].setNext(nodes[i + 1]);
+            }
+            if (i > 0) {
+                nodes[i].setPrevious(nodes[i - 1]);
+            }
+        }
+        //nodes[length - 2].data = 9; // Uncomment to ruin palindrome
+
+        LinkedListNode head = nodes[0];
+        System.out.println(head.printForward());
+        System.out.println(isPalindrome(head));
+    }
+
 
     public static class QuestionB {
         public static boolean isPalindrome(LinkedListNode head) {
             LinkedListNode fast = head;
             LinkedListNode slow = head;
 
-            Stack<Integer> stack = new Stack<Integer>();
+            Stack<Integer> stack = new Stack<>();
 
             while (fast != null && fast.next != null) {
                 stack.push(slow.data);
